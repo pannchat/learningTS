9f0e6d89625c67bba135ff9bd0d66900f5226dc4
pannchat <pannchat@likelion.org>
2021-10-24 20:17:21 +0900

    ts

--- index.ts
+++ index.ts
@@ -1,3 +1,32 @@
 let name1 = "kim";
-let name2 :string = "park";
-name2 = 123
+let name2 : string = "park";
+// name2 = 123;
+
+// let arr : string[] = [123, 'park'];
+let obj : { name : string } = {name : 'park'}
+let name3 : string | number = 'park'
+
+type MyType = string | number;
+let name4 : MyType = 123;
+
+function func(x : number) : number{
+    return x*2;
+}
+
+// type Member = [number, boolean];
+// let john:Member = [123,true];
+
+type Member = {
+    name : string
+}
+
+let john : Member = {
+    name : 'kim'
+}
+
+class User{
+    name :string ;
+    constructor(name :string ){
+        this.name = name;
+    }
+}
